727ce6e3acc10b07e2d3aaa29496731404bd9590
sobbinLee <sooblair84@com>
2020-11-02 14:52:53 +0900

    more adding admin pannel

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
+# @admin.register(models.User) 가 뜻하는 의미
+# admin패널에서 이 user를 보고싶을때 user를 컨트롤한 클래스가 바로 이게 될것이다.
+# 이것을 decorator라고하는데 class 위에 항상 위치 해 있어야 작동한다.
+
+
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin"""
+
+    # list_display = ("username", "gender", "language", "email", "currency", "superhost")
+    # list_filter = ("superhost", "currency", "language")
 
 
 # server가 실행되지 않아서 추가한 옵션
--- users/models.py
+++ users/models.py
@@ -2,20 +2,27 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+# mgriation 하는법
+# python filename.py migrations
+# python filename.py migrate
+
+# 데이터베이스에 default 값을 넣어줘야
+# Migration 했을때 빈공간없이 default 값으로 채워준다.아니면 null 값으로 채운다.
+# 이와같이 하면 column이 설정되기전에 값들이 채워진다.
 
 
 class User(AbstractUser):
 
     """ Custome User Model """
 
-    GENDER_FEMAIL = "female"
     GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
-    GENDER_CHOICES = (
-        (GENDER_FEMAIL, "female"),
-        (GENDER_MALE, "male"),
-        (GENDER_OTHER, "other"),
+    GENDER_CHOICE = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
     )
 
     LANGUAGE_ENGLISH = "en"
@@ -28,8 +35,13 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
+    # image 데이터를 처리하는 pillow(파이썬 이미지 라이브러리) 필수 설치
+    # pipenv install pillow
+    # blank는 웹프론트 단의 null 이라생각하면된다
+    # null 값을 허용해도 프론트에서는 필수사항이라는 이유가 db에서만 null 허용이기에 blank를 써야 완벽한 null허용이 된다.
+    # blank를 설정해야 form에 적용된다.
     avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    gender = models.CharField(choices=GENDER_CHOICE, max_length=10, null=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
