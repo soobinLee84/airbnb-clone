fdb065517b3687ad570e2a9685e233b17cfb78c4
soobin <sooblair84@gmail.com>
2020-11-17 22:38:34 +0900

    see_rooms and make fake db

---
+++ rooms/management/commands/seed_rooms.py
@@ -0,0 +1,27 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from rooms import models as room_models
+from users import models as user_models
+
+
+class Command(BaseCommand):
+
+    help = "This Command creates many users"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",default=2, type=int, help="How many rooms do you wnat to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(room_models.Room, number,{
+            'host': lambda x: random.choice(all_users),
+            'room_type' : lambda x: random.choice(room_types)
+        })
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
